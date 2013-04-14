e2e4eb371208cc7070624d1b46db4e30adcfe9c1
Carlos de Oliveira Junior <vamoss@vamoss.com.br>
2013-04-14 20:20:13 -0300

    KinectUI: add select all and unselect all buttons for joints

--- of_v0.7.4_vs2010_release/apps/vamoss/dancePerformancePanel/src/kinectUI.h
+++ of_v0.7.4_vs2010_release/apps/vamoss/dancePerformancePanel/src/kinectUI.h
@@ -26,24 +26,26 @@ public:
         float xInit = OFX_UI_GLOBAL_WIDGET_SPACING; 
         float length = 435-xInit; 
         
-        gui1 = new ofxUICanvas(405*3, 0, length, ofGetHeight());
+		gui1 = new ofxUICanvas(405*3, 0, length, ofGetHeight());
 		gui1->setName("KinectUI");
 		gui1->setWidgetSpacing(20);
-        gui1->addWidgetDown(new ofxUILabel("KINECT", OFX_UI_FONT_MEDIUM));
-        gui1->addWidgetDown(new ofxUISlider(length-xInit, dim, 0.0, 10.0, 3.0, "SCALE"));
-        gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "HEAD"));	
-        gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "SHOULDER LEFT"));
-        gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "SHOULDER CENTER"));
-        gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "SHOULDER RIGHT"));
-        gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "ELBOW LEFT"));
-        gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "ELBOW RIGHT"));
+		gui1->addWidgetDown(new ofxUILabel("KINECT", OFX_UI_FONT_MEDIUM));
+		gui1->addWidgetDown(new ofxUISlider(length-xInit, dim, 0.0, 10.0, 3.0, "SCALE"));
+		gui1->addWidgetDown(new ofxUILabelButton( length/2-xInit, false, "SELECT ALL", OFX_UI_FONT_MEDIUM)); 	
+		gui1->addWidgetRight(new ofxUILabelButton( length/2-xInit, false, "UNSELECT ALL", OFX_UI_FONT_MEDIUM)); 	
+		gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "HEAD"));	
+		gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "SHOULDER LEFT"));
+		gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "SHOULDER CENTER"));
+		gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "SHOULDER RIGHT"));
+		gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "ELBOW LEFT"));
+		gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "ELBOW RIGHT"));
 		gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "WRIST LEFT"));
 		gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "WRIST RIGHT"));
 		gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "HAND LEFT"));
 		gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "HAND RIGHT"));
-        gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "SPINE"));
+		gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "SPINE"));
 		gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "HIP LEFT"));
-        gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "HIP CENTER"));
+		gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "HIP CENTER"));
 		gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "HIP RIGHT"));
 		gui1->addWidgetDown(new ofxUIToggle( dim, dim, true, "KNEE LEFT"));
 		gui1->addWidgetRight(new ofxUIToggle( dim, dim, true, "KNEE RIGHT"));
@@ -91,6 +93,17 @@ public:
 		else if(name == "SAVE")
         {
             save();
+        }
+		else if(name == "SELECT ALL" || name == "UNSELECT ALL")
+        {
+			bool select = name == "SELECT ALL";
+
+			vector<ofxUIWidget*> toggles = gui1->getWidgetsOfType(OFX_UI_WIDGET_TOGGLE);
+			for(vector<ofxUIWidget*>::iterator it = toggles.begin(); it != toggles.end(); it++) {
+				ofxUIToggle* toggle = (ofxUIToggle *) *it;
+				toggle->setValue(select);
+				server::send(toggle->getName(), toggle->getValue());
+			}
         }
 		else
 		{
